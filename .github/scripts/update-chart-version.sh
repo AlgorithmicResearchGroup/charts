3f9cfa880557f906e400bbd7d68635b7135fc04b
Brad Moulder <bradley.moulder@gmail.com>
2025-07-26 19:46:26 -0500

    fix: testing pipeline to publish helm v0

--- .github/scripts/update-chart-version.sh
+++ .github/scripts/update-chart-version.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
-VERSION=$1
+VERSION="$1"
 
-if [ -z "$VERSION" ]; then
-  echo "Version not passed"
+if [[ -z "$VERSION" ]]; then
+  echo "Missing chart version"
   exit 1
 fi
 
-CHART_FILE="Chart.yaml"
-
-if [ ! -f "$CHART_FILE" ]; then
-  echo "$CHART_FILE not found"
-  exit 1
-fi
-
-# Update version in Chart.yaml
-yq e -i ".version = \"$VERSION\"" "$CHART_FILE"
+# Update version field in Chart.yaml
+yq e -i ".version = \"$VERSION\"" Chart.yaml
